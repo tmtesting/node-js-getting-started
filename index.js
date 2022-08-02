b025ec5865159cb0bd92110f479f8631c4bf5600
teromeri <teromeri@hotmail.com>
2022-08-02 23:29:57 +0300

    Note model

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 require('dotenv').config()
-// const { Sequelize } = require('sequelize')
-const { Sequelize, QueryTypes } = require('sequelize')
+// const { Sequelize, QueryTypes } = require('sequelize')
+const { Sequelize, Model, DataTypes } = require('sequelize')
 const express = require('express')
 const app = express()
+app.use(express.json())
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialectOptions: {
@@ -13,10 +14,41 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   },
 })
 
+class Note extends Model {}
+Note.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  content: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  important: {
+    type: DataTypes.BOOLEAN
+  },
+  date: {
+    type: DataTypes.DATE
+  }
+}, {
+  sequelize,
+  underscored: true,
+  timestamps: false,
+  modelName: 'note'
+})
 
 app.get('/api/notes', async (req, res) => {
-  const notes = await sequelize.query("SELECT * FROM notes", { type: QueryTypes.SELECT })
+  // const notes = await sequelize.query("SELECT * FROM notes", { type: QueryTypes.SELECT })
+  const notes = await Note.findAll()
   res.json(notes)
+})
+
+
+app.post('/api/notes', async (req, res) => {
+  console.log(req.body)
+  const note = await Note.create(req.body)
+  res.json(note)
 })
 
 const PORT = process.env.PORT || 3001
