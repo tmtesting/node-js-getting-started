cb059caa03cb38ad3e3d6a0a025d1de7b0df67e8
teromeri <teromeri@hotmail.com>
2022-08-02 23:12:02 +0300

    get api notes

--- index.js
+++ index.js
@@ -13,6 +13,19 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   },
 })
 
+
+app.get('/api/notes', async (req, res) => {
+  const notes = await sequelize.query("SELECT * FROM notes", { type: QueryTypes.SELECT })
+  res.json(notes)
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
+
+
+
 // const main = async () => {
 //   try {
 //     await sequelize.authenticate()
@@ -24,15 +37,6 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
 //     console.error('Unable to connect to the database:', error)
 //   }
 // }
-
-app.get('/api/notes', async (req, res) => {
-  const notes = await sequelize.query("SELECT * FROM notes", { type: QueryTypes.SELECT })
-  res.json(notes)
-})
-const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
 
 // main()
 
